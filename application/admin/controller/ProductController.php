b7a2f7dbd9480effed944398db9e0ce883dd33ad
beijixing <long_555@126.com>
2017-07-08 23:48:00 +0800

    a

--- application/admin/controller/ProductController.php
+++ application/admin/controller/ProductController.php
@@ -45,14 +45,14 @@ class ProductController extends BaseController
         $error = $file['error'];
         $path = ROOT_PATH.'public'.DS.'uploads/';
 
-        if($error !=0 ){//返回代码不为0是表示上传失败，为0则为成功
-            $msg['statusCode'] = 0;
-            $msg['message'] = '上传文件失败！';
-        }else{
-            move_uploaded_file($tmpName, $path. $name);
-            $msg['statusCode'] = 1;
-            $msg['message'] = '上传文件成功！';
-        }
+//        if($error !=0 ){//返回代码不为0是表示上传失败，为0则为成功
+//            $msg['statusCode'] = 0;
+//            $msg['message'] = '上传文件失败！';
+//        }else{
+//            move_uploaded_file($tmpName, $path. $name);
+//            $msg['statusCode'] = 1;
+//            $msg['message'] = '上传文件成功！';
+//        }
         return json( ['files' => $_FILES ,'file'=> $file, 'name'=>$name, 'type'=>gettype($_FILES)]);
 
     }
