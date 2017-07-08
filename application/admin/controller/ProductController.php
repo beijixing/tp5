9f48c8475ca68ed31595c292aa6ad040fae6ae0d
beijixing <long_555@126.com>
2017-07-08 23:20:54 +0800

    a

--- application/admin/controller/ProductController.php
+++ application/admin/controller/ProductController.php
@@ -36,20 +36,24 @@ class ProductController extends BaseController
 
 
     public function upload() {
-        $file = $_FILES['file'];
+
+
+        $file= $_FILES['file'];
         $name = $file['name'];
         $tmpName = $file['tmp_name'];
         $error = $file['error'];
         $path = ROOT_PATH.'public'.DS.'uploads/';
-        if($error){//返回代码不为0是表示上传失败，为0则为成功
-            $msg['statusCode'] = 0;
-            $msg['message'] = '上传文件失败！';
-        }else{
-            move_uploaded_file($tmpName, $path. $name);
-            $msg['statusCode'] = 1;
-            $msg['message'] = '上传文件成功！';
-        }
-//        return json( ['files' => $_FILES ,'file'=> $file, 'name'=>$name, 'type'=>gettype($_FILES)]);
+        echo  $tmpName;
+        echo  $error;
+//        if($error){//返回代码不为0是表示上传失败，为0则为成功
+//            $msg['statusCode'] = 0;
+//            $msg['message'] = '上传文件失败！';
+//        }else{
+//            move_uploaded_file($tmpName, $path. $name);
+//            $msg['statusCode'] = 1;
+//            $msg['message'] = '上传文件成功！';
+//        }
+        return json( ['files' => $_FILES ,'file'=> $file, 'name'=>$name, 'type'=>gettype($_FILES)]);
 
     }
 
