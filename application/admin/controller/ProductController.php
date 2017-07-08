e5f19fd86d4f8a36ee7f4b89dec88348631cba3b
beijixing <long_555@126.com>
2017-07-08 19:52:44 +0800

    aa

--- application/admin/controller/ProductController.php
+++ application/admin/controller/ProductController.php
@@ -50,19 +50,39 @@ class ProductController extends BaseController
 //        }
 
 
-        return json_encode(['files' => $_FILES['file'], 'post'=> $request->post()]);
-//        $file = $_FILES['files'];
-//        $path = ROOT_PATH.'public'.DS.'uploads/';
-//        $msg = nFileUpload($file, $path);
-//
-//        if ($msg['statusCode'] == 0) {
-//            return json( ['error' => $msg['message'] ]);
-//        }else {
-//
-//            $fileAbsolutePath = $request->domain(). dirname($_SERVER['SCRIPT_NAME']).'/public/uploads/'.$msg['dst'];
-//
-//            return json( ['fileName' => $fileAbsolutePath]);
-//        }
+        $file = $_FILES;
+        $path = ROOT_PATH.'public'.DS.'uploads/';
+        $msg = $this->nFileUpload($file, $path);
+
+        if ($msg['statusCode'] == 0) {
+            return json( ['error' => $msg['message'] ]);
+        }else {
+
+            $fileAbsolutePath = $request->domain(). dirname($_SERVER['SCRIPT_NAME']).'/public/uploads/'.$msg['dst'];
+
+            return json( ['fileName' => $fileAbsolutePath]);
+        }
     }
+
+    function nFileUpload($file, $path, $saveName = false){//函数会默认将同名文件覆盖
+        $dst = '';
+        if($file['file']['error']){//返回代码不为0是表示上传失败，为0则为成功
+            $msg['statusCode'] = 0;
+            $msg['message'] = '上传文件失败！';
+        }else{
+            if($saveName == false){//如果保存文件名为空，则将上传的文件移动到相应目录
+                $dst = $file['file']['name'];
+                move_uploaded_file($file['file']['tmp_name'], $path.$file['file']['name']);
+            }else{
+                $arr = explode(".", $file['file']['name']);//如果保存文件名不为空，则将上传的文件移动到相应目录，并按指定文件名命名
+                move_uploaded_file($file['file']['tmp_name'], $path.$saveName.".".end($arr));
+            }
+            $msg['statusCode'] = 1;
+            $msg['message'] = '上传文件成功！';
+            $msg['dst'] = $dst;
+        }
+        return $msg;
+    }
+
 
 }
