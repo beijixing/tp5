8d830c9330fa5bb4fe7cede73280105fe997b520
beijixing <long_555@126.com>
2017-07-08 20:06:45 +0800

    a

--- application/admin/controller/ProductController.php
+++ application/admin/controller/ProductController.php
@@ -50,39 +50,25 @@ class ProductController extends BaseController
 //        }
 
 //        return json( ['files' => $_FILES]);
-        $files = $_FILES;
         $path = ROOT_PATH.'public'.DS.'uploads/';
-        $msg = $this->nFileUpload($files, $path);
+        $dst = $_FILES['file']['name'];
+        if($_FILES['file']['error']){//返回代码不为0是表示上传失败，为0则为成功
+            $msg['statusCode'] = 0;
+            $msg['message'] = '上传文件失败！';
+        }else{
+            move_uploaded_file($_FILES['file']['tmp_name'], $path. $dst);
+            $msg['statusCode'] = 1;
+            $msg['message'] = '上传文件成功！';
+        }
 
         if ($msg['statusCode'] == 0) {
             return json( ['error' => $msg['message'] ]);
         }else {
-
-            $fileAbsolutePath = $request->domain(). dirname($_SERVER['SCRIPT_NAME']).'/public/uploads/'.$msg['dst'];
-
+            $fileAbsolutePath = $request->domain(). dirname($_SERVER['SCRIPT_NAME']).'/public/uploads/'.$dst;
             return json( ['fileName' => $fileAbsolutePath]);
         }
     }
 
-    function nFileUpload($file, $path, $saveName = false){//函数会默认将同名文件覆盖
-        $dst = '';
-        if($file['file']['error']){//返回代码不为0是表示上传失败，为0则为成功
-            $msg['statusCode'] = 0;
-            $msg['message'] = '上传文件失败！';
-        }else{
-            if($saveName == false){//如果保存文件名为空，则将上传的文件移动到相应目录
-                $dst = $file['file']['name'];
-                move_uploaded_file($file['file']['tmp_name'], $path.$file['file']['name']);
-            }else{
-                $arr = explode(".", $file['file']['name']);//如果保存文件名不为空，则将上传的文件移动到相应目录，并按指定文件名命名
-                move_uploaded_file($file['file']['tmp_name'], $path.$saveName.".".end($arr));
-            }
-            $msg['statusCode'] = 1;
-            $msg['message'] = '上传文件成功！';
-            $msg['dst'] = $dst;
-        }
-        return $msg;
-    }
 
 
 }
--- application/common.php
+++ application/common.php
@@ -12,5 +12,22 @@
 // 应用公共文件
 
 
-
-
+function nFileUpload($file, $path, $saveName = false){//函数会默认将同名文件覆盖
+    $dst = '';
+    if($file['file']['error']){//返回代码不为0是表示上传失败，为0则为成功
+        $msg['statusCode'] = 0;
+        $msg['message'] = '上传文件失败！';
+    }else{
+        if($saveName == false){//如果保存文件名为空，则将上传的文件移动到相应目录
+            $dst = $file['file']['name'];
+            move_uploaded_file($file['file']['tmp_name'], $path.$file['file']['name']);
+        }else{
+            $arr = explode(".", $file['file']['name']);//如果保存文件名不为空，则将上传的文件移动到相应目录，并按指定文件名命名
+            move_uploaded_file($file['file']['tmp_name'], $path.$saveName.".".end($arr));
+        }
+        $msg['statusCode'] = 1;
+        $msg['message'] = '上传文件成功！';
+        $msg['dst'] = $dst;
+    }
+    return $msg;
+}
