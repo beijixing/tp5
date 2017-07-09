911976038502103fb5ec91e6ebf40de2799e309a
beijixing <long_555@126.com>
2017-07-09 11:05:56 +0800

    a

--- application/admin/controller/ProductController.php
+++ application/admin/controller/ProductController.php
@@ -43,19 +43,48 @@ class ProductController extends BaseController
         $tmpName = $file['tmp_name'];
         $error = $file['error'];
         $path = ROOT_PATH.'public'.DS.'uploads/';
-
+        $msg = array();
         if($error != 0 ){//返回代码不为0是表示上传失败，为0则为成功
             $msg['statusCode'] = 0;
-            $msg['message'] = '上传文件失败！';
+            switch($error) {
+                case 1:
+                    $msg['message'] = "文件大小超出了服务器的空间大小 ";
+                    break;
+
+                case 2:
+                    $msg['message'] = "要上传的文件大小超出浏览器限制 ";
+                    break;
+
+                case 3:
+                    $msg['message'] = "文件仅部分被上传";
+                    break;
+
+                case 4:
+                    $msg['message'] = "没有找到要上传的文件";
+                    break;
+
+                case 5:
+                    $msg['message'] = "服务器临时文件夹丢失";
+                    break;
+
+                case 6:
+                     $msg['message'] = "文件写入到临时文件夹出错";
+                    break;
+            }
         }else{
             move_uploaded_file($tmpName, $path. $name);
             $msg['statusCode'] = 1;
             $msg['message'] = '上传文件成功！';
         }
-        return json( ['files' => $_FILES ,'file'=> $_POST, 'name'=>"name", 'type'=>gettype($_FILES)]);
+
+        if ($msg['statusCode'] = 1) {
+            //成功上传图片
+            $fileAbsolutePath = $request->domain(). dirname($_SERVER['SCRIPT_NAME']).'/public/uploads/'.$name;
+            return json(['fileName' => $fileAbsolutePath]);
+        }else {
+            return json( ['error' => $msg['message'] ]);
+        }
 
     }
-
-
 
 }
